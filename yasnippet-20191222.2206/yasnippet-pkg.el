57dfffd965215f07dfc095547bf5e535d060ca68
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-04-01 08:34:09 +0200

    yasnippet-20191222.2206 -> yasnippet-20200329.1434

--- yasnippet-20191222.2206/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/peterg/.emacs.d/elpa/yasnippet-20191222.2206/yasnippet.el  -*- no-byte-compile: t -*-
-(define-package "yasnippet" "20191222.2206" "Yet another snippet extension for Emacs" '((cl-lib "0.5")) :commit "ac03c2f192ac0a6e8fca8c5b93a0eb7955e43c24" :keywords '("convenience" "emulation") :maintainer '("Noam Postavsky" . "npostavs@gmail.com") :url "http://github.com/joaotavora/yasnippet")
