cadb51318594c7b2004d08d1371abf8bb94402f0
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-04-01 08:25:17 +0200

    new: which-key-3.4.0

---
+++ which-key-3.4.0/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/peterg/.emacs.d/elpa/which-key-3.4.0/which-key.el  -*- no-byte-compile: t -*-
+(define-package "which-key" "3.4.0" "Display available keybindings in popup" '((emacs "24.4")) :commit "1e3640e48c31f8062f018b5fc84acad696a0ea2a" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
