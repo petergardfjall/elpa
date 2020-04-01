378d272a79f3438d7f199ea70bb7de3a1b205c31
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-04-01 08:25:17 +0200

    new: protobuf-mode-20170526.1650

---
+++ protobuf-mode-20170526.1650/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/peterg/.emacs.d/elpa/protobuf-mode-20170526.1650/protobuf-mode.el  -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.1650" "major mode for editing protocol buffers." 'nil :commit "dec4939439d9ca2adf2bb14edccf876c2587faf2" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
