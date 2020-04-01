a21c590ff1226a600c02aa96d910d52f93f7da47
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-04-01 08:25:17 +0200

    new: graphql-mode-20191024.1221

---
+++ graphql-mode-20191024.1221/graphql-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/peterg/.emacs.d/elpa/graphql-mode-20191024.1221/graphql-mode.el  -*- no-byte-compile: t -*-
+(define-package "graphql-mode" "20191024.1221" "Major mode for editing GraphQL schemas" '((emacs "24.3")) :commit "7c37aee28bf8c8ffb3da73df5571c4e1e352562b" :keywords '("languages") :authors '(("David Vazquez Pua" . "davazp@gmail.com")) :maintainer '("David Vazquez Pua" . "davazp@gmail.com"))
