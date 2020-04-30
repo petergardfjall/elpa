95613c34026f62a5fc675b54c4391f60b66cbf5b
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-04-30 07:23:16 +0200

    rust-mode-20200427.848 -> rust-mode-20200429.213

--- rust-mode-20200427.848/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/peterg/.emacs.d/elpa/rust-mode-20200427.848/rust-mode.el  -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20200427.848" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "1f8a7631da96423fc08734fcee8db6055a3d0e31" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
