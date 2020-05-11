368a5ca71e7b8ae778f57553c62b73ac954e9035
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-05-11 08:02:53 +0200

    yasnippet-snippets-20200425.1210 -> yasnippet-snippets-20200508.936

--- yasnippet-snippets-20200508.936/yasnippet-snippets-pkg.el
+++ yasnippet-snippets-20200508.936/yasnippet-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yasnippet-snippets" "20200425.1210" "Collection of yasnippet snippets"
+(define-package "yasnippet-snippets" "20200508.936" "Collection of yasnippet snippets"
   '((yasnippet "0.8.0"))
   :keywords
   '("snippets")
