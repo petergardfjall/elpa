c022e46e58867d38d632eae3ade03734edb1abec
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-05-11 08:02:53 +0200

    yaml-mode-20191127.2314 -> yaml-mode-20200510.1921

---
+++ yaml-mode-20200510.1921/yaml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/peterg/.emacs.d/elpa/yaml-mode-20200510.1921/yaml-mode.el  -*- no-byte-compile: t -*-
+(define-package "yaml-mode" "20200510.1921" "Major mode for editing YAML files" '((emacs "24.1")) :commit "9e8362dc553cde6f5ff27c4dd9b119200445819f" :keywords '("data" "yaml") :authors '(("Yoshiki Kurihara" . "clouder@gmail.com") ("Marshall T. Vandegrift" . "llasram@gmail.com")) :maintainer '("Vasilij Schneidermann" . "mail@vasilij.de"))
