2e1e020c8616bbb14d3c067838bbb06066e16e95
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-05-18 08:05:58 +0200

    treemacs-20200506.1600 -> treemacs-20200516.1204

--- treemacs-20200516.1204/treemacs-autoloads.el
+++ treemacs-20200516.1204/treemacs-autoloads.el
@@ -197,8 +197,9 @@ If SIZE is 'nil' the icons are not resized and will retain their default size of
 There is only one size, the icons are square and the aspect ratio will be
 preserved when resizing them therefore width and height are the same.
 
-Resizing the icons only works if Emacs was built with ImageMagick support.  If
-this is not the case this function will not have any effect.
+Resizing the icons only works if Emacs was built with ImageMagick support, or if
+using Emacs >= 27.1,which has native image resizing support.  If this is not the
+case this function will not have any effect.
 
 Custom icons are not taken into account, only the size of treemacs' own icons
 png are changed.
--- treemacs-20200516.1204/treemacs-pkg.el
+++ treemacs-20200516.1204/treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "treemacs" "20200506.1600" "A tree style file explorer package"
+(define-package "treemacs" "20200516.1204" "A tree style file explorer package"
   '((emacs "25.2")
     (cl-lib "0.5")
     (dash "2.11.0")
