67b9957285073d2857dbce5bc63d72192e9840f8
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-05-18 08:05:57 +0200

    git-commit-20200207.1819 -> git-commit-20200516.2016

--- git-commit-20200516.2016/git-commit.el
+++ git-commit-20200516.2016/git-commit.el
@@ -12,7 +12,7 @@
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
 ;; Package-Requires: ((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103"))
-;; Package-Version: 20200207.1819
+;; Package-Version: 20200516.2016
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
@@ -697,7 +697,7 @@ With a numeric prefix ARG, go forward ARG comments."
 
 ;;; Headers
 
-(define-transient-command git-commit-insert-pseudo-header ()
+(transient-define-prefix git-commit-insert-pseudo-header ()
   "Insert a commit message pseudo header."
   [["Insert ... by yourself"
     ("a"   "Ack"         git-commit-ack)
