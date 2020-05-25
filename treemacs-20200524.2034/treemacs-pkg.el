e0961029dc3d1c56bceec26ab7e81ad7ba116fc9
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-05-25 07:51:59 +0200

    treemacs-20200516.1204 -> treemacs-20200524.2034

--- treemacs-20200524.2034/treemacs-pkg.el
+++ treemacs-20200524.2034/treemacs-pkg.el
@@ -1,8 +1,8 @@
-(define-package "treemacs" "20200516.1204" "A tree style file explorer package"
+(define-package "treemacs" "20200524.2034" "A tree style file explorer package"
   '((emacs "25.2")
     (cl-lib "0.5")
     (dash "2.11.0")
-    (s "1.10.0")
+    (s "1.12.0")
     (f "0.11.0")
     (ace-window "0.9.0")
     (pfuture "1.7")
