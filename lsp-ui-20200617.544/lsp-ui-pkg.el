f092aea68fcdde0678d852a4eb95955bd824e2de
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-06-17 14:38:48 +0200

    lsp-ui-20200607.2009 -> lsp-ui-20200617.544

--- lsp-ui-20200617.544/lsp-ui-pkg.el
+++ lsp-ui-20200617.544/lsp-ui-pkg.el
@@ -1,10 +1,10 @@
-(define-package "lsp-ui" "20200607.2009" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20200617.544" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.14")
     (dash-functional "1.2.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "d92cf83d95c9ca177b735500ead88cf68dc2e2db" :keywords
+  :commit "ce997c4dabb494ec4aaa93373ae27cd4d5cd0a4d" :keywords
   '("languages" "tools")
   :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
