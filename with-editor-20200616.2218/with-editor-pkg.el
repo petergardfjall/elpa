25e08db4b53d1d8329f6fc912a630e4bff6d54bb
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-06-17 14:38:48 +0200

    with-editor-20200612.2210 -> with-editor-20200616.2218

--- with-editor-20200616.2218/with-editor-pkg.el
+++ with-editor-20200616.2218/with-editor-pkg.el
@@ -1,7 +1,7 @@
-(define-package "with-editor" "20200612.2210" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20200616.2218" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
     (async "1.9"))
-  :commit "48ca9bb49a1a7a37e85606de9d327a14030d4380" :keywords
+  :commit "f2a9b020fa8f3333bf46ecf9f8c164e8655e3c72" :keywords
   '("tools")
   :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
