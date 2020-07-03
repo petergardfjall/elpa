8109d9dff2a1fcc14e5aa7a548030aa42e3303e4
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-07-03 11:10:39 +0200

    ccls-20200327.1915 -> ccls-20200701.546

--- ccls-20200701.546/ccls-pkg.el
+++ ccls-20200701.546/ccls-pkg.el
@@ -1,9 +1,9 @@
-(define-package "ccls" "20200327.1915" "ccls client for lsp-mode"
+(define-package "ccls" "20200701.546" "ccls client for lsp-mode"
   '((emacs "25.1")
-    (lsp-mode "4.2")
-    (dash "0.14")
+    (lsp-mode "6.3.1")
+    (dash "2.14.1")
     (projectile "1.0.0"))
-  :keywords
+  :commit "914516d6ce7b1864b1dd479c79d682ea80916df9" :keywords
   '("languages" "lsp" "c++")
   :authors
   '(("Tobias Pisani, Fangrui Song"))
