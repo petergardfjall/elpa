1f61f52e3123f51912dbac50874e90e546d60ff2
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-07-03 11:10:39 +0200

    protobuf-mode-20200520.15 -> protobuf-mode-20200619.1742

--- protobuf-mode-20200520.15/protobuf-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/peterg/.emacs.d/elpa/protobuf-mode-20200520.15/protobuf-mode.el  -*- no-byte-compile: t -*-
-(define-package "protobuf-mode" "20200520.15" "major mode for editing protocol buffers." 'nil :commit "39d730dd96c81196893734ee1e075c34567e59ae" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
--- protobuf-mode-20200619.1742/protobuf-mode.el
+++ protobuf-mode-20200619.1742/protobuf-mode.el
@@ -3,7 +3,8 @@
 ;; Author: Alexandre Vassalotti <alexandre@peadrop.com>
 ;; Created: 23-Apr-2009
 ;; Version: 0.3
-;; Package-Version: 20200520.15
+;; Package-Version: 20200619.1742
+;; Package-Commit: 86b3ccf28ca437330cc42a2b3a75a1314977fcfd
 ;; Keywords: google protobuf languages
 
 ;; Redistribution and use in source and binary forms, with or without
@@ -217,7 +218,11 @@ Key bindings:
   (c-common-init 'protobuf-mode)
   (easy-menu-add protobuf-menu)
   (c-run-mode-hooks 'c-mode-common-hook 'protobuf-mode-hook)
-  (c-update-modeline))
+  (c-update-modeline)
+  (setq imenu-generic-expression
+	    '(("Message" "^[[:space:]]*message[[:space:]]+\\([[:alnum:]]+\\)" 1)
+          ("Enum" "^[[:space:]]*enum[[:space:]]+\\([[:alnum:]]+\\)" 1)
+          ("Service" "^[[:space:]]*service[[:space:]]+\\([[:alnum:]]+\\)" 1))))
 
 (provide 'protobuf-mode)
 
