e3c12f6bbf28f7aea6a6ce38e2b8da62de0be12a
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-07-13 11:51:49 +0200

    magit-20200702.1506 -> magit-20200710.433

--- magit-20200710.433/magit-pkg.el
+++ magit-20200710.433/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20200702.1506" "A Git porcelain inside Emacs."
+(define-package "magit" "20200710.433" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20200113")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "485ee181564655d21c0770a4e021f7b805f4d643" :keywords
+  :commit "312791e50bb56797c1ee36f4aa3fdc0885711b25" :keywords
   '("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
