596a30fd66a75117fd7d926e5c471353a91b04ce
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-07-21 10:21:39 +0200

    new: cmake-mode-20190710.1319

---
+++ cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/peterg/.emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "cd580a99bdaa658487d7080279c9d394545c4f10")
