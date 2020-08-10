51f972b5023091871838077b0ed6a25aef93afb4
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-08-10 06:31:18 +0200

    lsp-mode-20200728.1517 -> lsp-mode-20200809.1551

---
+++ lsp-mode-20200809.1551/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20200809.1551" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "8b7cc83472bb9df8bce2d55ec37afe3c7ddc6be2" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
