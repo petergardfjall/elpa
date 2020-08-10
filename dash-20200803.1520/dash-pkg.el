9f1b2c9b75ef198c4ad88d0832cc2b7ed1039631
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-08-10 06:31:18 +0200

    dash-20200524.1947 -> dash-20200803.1520

--- dash-20200803.1520/dash-pkg.el
+++ dash-20200803.1520/dash-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dash" "20200524.1947" "A modern list library for Emacs" 'nil :keywords
+(define-package "dash" "20200803.1520" "A modern list library for Emacs" 'nil :commit "b92ab5a39b987e4fe69317b9d9fda452300baf20" :keywords
   '("lists")
   :authors
   '(("Magnar Sveen" . "magnars@gmail.com"))
