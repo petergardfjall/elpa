d1e3dffb44a5b9a95b5d20d3618a3edff8c233bb
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-08-10 06:31:18 +0200

    yasnippet-snippets-20200606.1149 -> yasnippet-snippets-20200802.1658

--- yasnippet-snippets-20200802.1658/yasnippet-snippets-pkg.el
+++ yasnippet-snippets-20200802.1658/yasnippet-snippets-pkg.el
@@ -1,7 +1,7 @@
-(define-package "yasnippet-snippets" "20200606.1149" "Collection of yasnippet snippets"
+(define-package "yasnippet-snippets" "20200802.1658" "Collection of yasnippet snippets"
   '((yasnippet "0.8.0")
     (s "1.12.0"))
-  :commit "d5ef8ed2b34798c1576f2fbfed858ee5486d1792" :keywords
+  :commit "b83c0f2f3bd068aa93c8f4c0c07f0b0a137d9cda" :keywords
   '("snippets")
   :authors
   '(("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
