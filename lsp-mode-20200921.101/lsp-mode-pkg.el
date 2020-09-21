7e3cc5502cdc614ac1b8f845947d7762739e6c54
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-09-21 08:21:51 +0200

    lsp-mode-20200916.2251 -> lsp-mode-20200921.101

--- lsp-mode-20200921.101/lsp-mode-pkg.el
+++ lsp-mode-20200921.101/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20200916.2251" "LSP mode"
+(define-package "lsp-mode" "20200921.101" "LSP mode"
   '((emacs "26.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
@@ -7,7 +7,7 @@
     (spinner "1.7.3")
     (markdown-mode "2.3")
     (lv "0"))
-  :commit "6741a5667587ab20a0ad19458a6773f0227596fb" :keywords
+  :commit "c01dab7acb80b08da1574cb07f729e8881771fe8" :keywords
   '("languages")
   :authors
   '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
