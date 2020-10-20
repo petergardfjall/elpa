92eccf1916c6f39aa565098dc7c56ecf85a3a213
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-10-20 12:26:18 +0200

    immaterial-theme-20200818.638 -> immaterial-theme-20201019.533

--- immaterial-theme-20201019.533/immaterial-theme-pkg.el
+++ immaterial-theme-20201019.533/immaterial-theme-pkg.el
@@ -1,6 +1,6 @@
-(define-package "immaterial-theme" "20200818.638" "A flexible theme based on material design principles"
+(define-package "immaterial-theme" "20201019.533" "A flexible theme based on material design principles"
   '((emacs "25"))
-  :commit "d7f18842f5e97977684c3dbb550c418b7625b343" :keywords
+  :commit "743cd287dd3f4634fb9f062f959a6a42c214383e" :keywords
   '("themes")
   :authors
   '(("Peter Gardfjäll"))
