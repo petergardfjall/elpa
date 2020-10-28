77cd44177f042930718d9b479a7aa16cb391dcf6
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-10-28 08:57:18 +0100

    treemacs-20201004.1125 -> treemacs-20201026.2006

--- treemacs-20201026.2006/treemacs-pkg.el
+++ treemacs-20201026.2006/treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "treemacs" "20201004.1125" "A tree style file explorer package"
+(define-package "treemacs" "20201026.2006" "A tree style file explorer package"
   '((emacs "25.2")
     (cl-lib "0.5")
     (dash "2.11.0")
@@ -8,7 +8,7 @@
     (pfuture "1.7")
     (hydra "0.13.2")
     (ht "2.2"))
-  :commit "6c00fe74079a057ad4efc772eab9d73e4f09fa10" :authors
+  :commit "ed7de5d0a25b19d9ad5f376b5e447123b418964b" :authors
   '(("Alexander Miller" . "alexanderm@web.de"))
   :maintainer
   '("Alexander Miller" . "alexanderm@web.de")
