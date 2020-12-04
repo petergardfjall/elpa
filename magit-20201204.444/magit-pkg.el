d79e3afe2c4fef9f1b6cf05aaaf3e58477fecd5d
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-12-04 08:11:23 +0100

    magit-20201127.1805 -> magit-20201204.444

--- magit-20201204.444/magit-pkg.el
+++ magit-20201204.444/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20201127.1805" "A Git porcelain inside Emacs."
+(define-package "magit" "20201204.444" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20200113")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "068f8389743b96d31069bb891b6cc26810bf50d9" :keywords
+  :commit "1f1a8c0de24899a217d45fcdf64439f14e3a540b" :keywords
   ("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
