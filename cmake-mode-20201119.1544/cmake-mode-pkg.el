5838b28aac2c04c0be5f41f59752aa8b4210d96b
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-12-14 07:42:54 +0100

    cmake-mode-20201119.1544 -> cmake-mode-20201210.1159

--- cmake-mode-20201119.1544/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/pgwrk/.emacs.d/elpa/cmake-mode-20201119.1544/cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20201119.1544" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "f85f86415b1ef628ef5ffc851733927be4098378")
---
+++ cmake-mode-20201210.1159/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/pgwrk/.emacs.d/elpa/cmake-mode-20201210.1159/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20201210.1159" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "0a6c7cdd07565e6a50ef69d4f2595ace238fa408")
