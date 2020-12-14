56a13c878748cb65c6a23c796bd0cdfc353a803f
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-12-14 07:42:55 +0100

    lsp-treemacs-20201122.844 -> lsp-treemacs-20201208.932

--- lsp-treemacs-20201208.932/lsp-treemacs-pkg.el
+++ lsp-treemacs-20201208.932/lsp-treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-treemacs" "20201122.844" "LSP treemacs"
+(define-package "lsp-treemacs" "20201208.932" "LSP treemacs"
   '((emacs "26.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
@@ -6,7 +6,7 @@
     (ht "2.0")
     (treemacs "2.5")
     (lsp-mode "6.0"))
-  :commit "e0486d7351bc9c794118210782ba2d918443ee7a" :keywords
+  :commit "642e922e16fd2b2c3cc075d7cba68448f1aaa006" :keywords
   ("languages")
   :authors
   (("Ivan Yonchovski"))
