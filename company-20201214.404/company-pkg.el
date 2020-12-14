96745db9cec5daf6b504978ab2ee1f8bb0cb7e04
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-12-14 07:42:54 +0100

    company-20201120.1115 -> company-20201214.404

--- company-20201214.404/company-pkg.el
+++ company-20201214.404/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20201120.1115" "Modular text completion framework"
+(define-package "company" "20201214.404" "Modular text completion framework"
   '((emacs "24.3"))
-  :commit "97cfbc3967c195fb4ccb171735b9b1dea97e681a" :keywords
+  :commit "75acbc54a46c47a48a061064295cf304d94108a7" :keywords
   ("abbrev" "convenience" "matching")
   :authors
   (("Nikolaj Schumacher"))
