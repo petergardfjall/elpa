dc2b107e698a78ca19131c6fdb7593cae4abd45e
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-01-11 07:33:49 +0100

    immaterial-theme-20201202.1801 -> immaterial-theme-20210109.1653

--- immaterial-theme-20210109.1653/immaterial-theme-autoloads.el
+++ immaterial-theme-20210109.1653/immaterial-theme-autoloads.el
@@ -28,7 +28,7 @@
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "immaterial-theme" '("immaterial-c")))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "immaterial-theme" '("immaterial-")))
 
 ;;;***
 
---
+++ immaterial-theme-20210109.1653/immaterial-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "immaterial-theme" "20210109.1653" "A flexible theme based on material design principles"
+  '((emacs "25"))
+  :commit "288b367ea0efccd5e98efbdf925591ffc989a654" :authors
+  '(("Peter Gardfjäll"))
+  :maintainer
+  '("Peter Gardfjäll")
+  :keywords
+  '("themes")
+  :url "https://github.com/petergardfjall/emacs-immaterial-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
