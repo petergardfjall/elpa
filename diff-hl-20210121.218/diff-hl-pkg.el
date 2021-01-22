bb61c998dab8791e91852b784ce1939bec58ed84
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-01-22 08:12:38 +0100

    diff-hl-20210113.1930 -> diff-hl-20210121.218

--- diff-hl-20210121.218/diff-hl-pkg.el
+++ diff-hl-20210121.218/diff-hl-pkg.el
@@ -1,7 +1,7 @@
-(define-package "diff-hl" "20210113.1930" "Highlight uncommitted changes using VC"
+(define-package "diff-hl" "20210121.218" "Highlight uncommitted changes using VC"
   '((cl-lib "0.2")
     (emacs "24.3"))
-  :commit "dc3e12f14f5fddff3811310cd48640e2c9e626da" :authors
+  :commit "3394d8536c315e9bf553d0ee0bdd5afa6c4aa6ff" :authors
   '(("Dmitry Gutov" . "dgutov@yandex.ru"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
