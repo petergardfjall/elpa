cdacb3e42fe7595b263130e86e31a542bb7471a0
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-01-25 08:16:52 +0100

    company-20210118.6 -> company-20210122.2314

--- company-20210122.2314/company-pkg.el
+++ company-20210122.2314/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20210118.6" "Modular text completion framework"
+(define-package "company" "20210122.2314" "Modular text completion framework"
   '((emacs "24.3"))
-  :commit "93415dcef014b8aad21633bae1dabecc28742df1" :authors
+  :commit "5c25e114c3ac1bee3671abd47f46592a3151d549" :authors
   '(("Nikolaj Schumacher"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
