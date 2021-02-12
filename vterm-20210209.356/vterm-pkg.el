d41d3602a0b639205d3ba8590fd8f5ed2c086586
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-02-12 08:08:05 +0100

    vterm-20210131.101 -> vterm-20210209.356

--- vterm-20210209.356/vterm-pkg.el
+++ vterm-20210209.356/vterm-pkg.el
@@ -1,6 +1,6 @@
-(define-package "vterm" "20210131.101" "Fully-featured terminal emulator"
+(define-package "vterm" "20210209.356" "Fully-featured terminal emulator"
   '((emacs "25.1"))
-  :commit "fb12d7f49e2da121d9b17ed583d269166e0da087" :authors
+  :commit "a3fadd28370aa43f03d4f7b197be8fa074f311f5" :authors
   '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
   :maintainer
   '("Lukas Fürmetz" . "fuermetz@mailbox.org")
