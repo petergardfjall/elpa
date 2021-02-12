6238be4339392e7a99b22d17f4fbe9956a2d9295
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-02-12 08:08:05 +0100

    markdown-preview-mode-20210129.1715 -> markdown-preview-mode-20210207.2114

--- markdown-preview-mode-20210207.2114/markdown-preview-mode-pkg.el
+++ markdown-preview-mode-20210207.2114/markdown-preview-mode-pkg.el
@@ -1,10 +1,10 @@
-(define-package "markdown-preview-mode" "20210129.1715" "markdown realtime preview minor mode."
+(define-package "markdown-preview-mode" "20210207.2114" "markdown realtime preview minor mode."
   '((emacs "24.3")
     (websocket "1.6")
     (markdown-mode "2.0")
     (cl-lib "0.5")
     (web-server "0.1.1"))
-  :commit "717d85a66612a69b378f70f8457a14b29d80192e" :authors
+  :commit "01af98ac1adcc9cb247fbc6c80fb159c30360ee6" :authors
   '(("Igor Shymko" . "igor.shimko@gmail.com"))
   :maintainer
   '("Igor Shymko" . "igor.shimko@gmail.com")
