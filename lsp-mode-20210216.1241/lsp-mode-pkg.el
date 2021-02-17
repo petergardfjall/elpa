0584b6d7fc7cd13aafee26a273f805b5cd1b9020
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-02-17 07:37:45 +0100

    lsp-mode-20210213.1526 -> lsp-mode-20210216.1241

--- lsp-mode-20210216.1241/lsp-mode-pkg.el
+++ lsp-mode-20210216.1241/lsp-mode-pkg.el
@@ -1,13 +1,12 @@
-(define-package "lsp-mode" "20210213.1526" "LSP mode"
+(define-package "lsp-mode" "20210216.1241" "LSP mode"
   '((emacs "26.1")
-    (dash "2.14.1")
-    (dash-functional "2.14.1")
+    (dash "2.18.0")
     (f "0.20.0")
     (ht "2.3")
     (spinner "1.7.3")
     (markdown-mode "2.3")
     (lv "0"))
-  :commit "ca6f01c34d3f005ea5f7acbabeacfef2a14b5c66" :authors
+  :commit "8166a1fe04891efcb7f1262609e77fc48500537b" :authors
   '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
   :maintainer
   '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
