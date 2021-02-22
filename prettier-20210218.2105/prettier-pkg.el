69797ad13302b82d2fd1f1bcc22d7526a08a0913
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-02-22 11:25:49 +0100

    prettier-20210129.826 -> prettier-20210218.2105

--- prettier-20210218.2105/prettier-pkg.el
+++ prettier-20210218.2105/prettier-pkg.el
@@ -1,8 +1,8 @@
-(define-package "prettier" "20210129.826" "Code formatting with Prettier"
+(define-package "prettier" "20210218.2105" "Code formatting with Prettier"
   '((emacs "26.1")
     (iter2 "0.9")
     (nvm "0.2"))
-  :commit "61f135a82156712b9226d9bf23156c0cce9d5c98" :authors
+  :commit "1b0bd4857862d4a1a667ccf2fd43cd9aba4d12d3" :authors
   '(("Julian Scheid" . "julians37@gmail.com"))
   :maintainer
   '("Julian Scheid" . "julians37@gmail.com")
