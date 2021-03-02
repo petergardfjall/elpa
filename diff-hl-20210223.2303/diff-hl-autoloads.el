0b05f73aaa320912dfdd55955a12df382e31e2f2
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-03-02 07:42:34 +0100

    diff-hl-20210213.41 -> diff-hl-20210223.2303

--- diff-hl-20210223.2303/diff-hl-autoloads.el
+++ diff-hl-20210223.2303/diff-hl-autoloads.el
@@ -27,13 +27,15 @@ Call `turn-on-diff-hl-mode' if the current major mode is applicable." nil nil)
 
 (autoload 'diff-hl-set-reference-rev "diff-hl" "\
 Set the reference revision globally to REV.
-When called interactively, REV is get from contexts:
+When called interactively, REV read with completion.
+
+The default value chosen using one of methods below:
 
 - In a log view buffer, it uses the revision of current entry.
 Call `vc-print-log' or `vc-print-root-log' first to open a log
 view buffer.
 - In a VC annotate buffer, it uses the revision of current line.
-- In other situations, get the revision name at point.
+- In other situations, it uses the symbol at point.
 
 Notice that this sets the reference revision globally, so in
 files from other repositories, `diff-hl-mode' will not highlight
@@ -43,7 +45,7 @@ Also notice that this will disable `diff-hl-amend-mode' in
 buffers that enables it, since `diff-hl-amend-mode' overrides its
 effect.
 
-\(fn &optional REV)" t nil)
+\(fn REV)" t nil)
 
 (autoload 'diff-hl-reset-reference-rev "diff-hl" "\
 Reset the reference revision globally to the most recent one." t nil)
@@ -168,6 +170,25 @@ toggle it if ARG is `toggle'; disable the mode otherwise.
 
 ;;;***
 
+;;;### (autoloads nil "diff-hl-inline-popup" "diff-hl-inline-popup.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from diff-hl-inline-popup.el
+
+(autoload 'diff-hl-inline-popup-hide "diff-hl-inline-popup" "\
+Hide the current inline popup." t nil)
+
+(autoload 'diff-hl-inline-popup-show "diff-hl-inline-popup" "\
+Create a phantom overlay to show the inline popup, with some
+content LINES, and a HEADER and a FOOTER, at POINT.  KEYMAP is
+added to the current keymaps.  CLOSE-HOOK is called when the popup
+is closed.
+
+\(fn LINES &optional HEADER FOOTER KEYMAP CLOSE-HOOK POINT HEIGHT)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-inline-popup" '("diff-hl-inline-popup-")))
+
+;;;***
+
 ;;;### (autoloads nil "diff-hl-margin" "diff-hl-margin.el" (0 0 0
 ;;;;;;  0))
 ;;; Generated autoloads from diff-hl-margin.el
@@ -204,6 +225,83 @@ and toggle it if ARG is `toggle'; disable the mode otherwise.
 \(fn &optional ARG)" t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-margin" '("diff-hl-")))
+
+;;;***
+
+;;;### (autoloads nil "diff-hl-show-hunk" "diff-hl-show-hunk.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from diff-hl-show-hunk.el
+
+(autoload 'diff-hl-show-hunk-inline-popup "diff-hl-show-hunk" "\
+Implementation to show the hunk in a inline popup.
+BUFFER is a buffer with the hunk.
+
+\(fn BUFFER &optional IGNORED-LINE)" nil nil)
+
+(autoload 'diff-hl-show-hunk-previous "diff-hl-show-hunk" "\
+Go to previous hunk/change and show it." t nil)
+
+(autoload 'diff-hl-show-hunk-next "diff-hl-show-hunk" "\
+Go to next hunk/change and show it." t nil)
+
+(autoload 'diff-hl-show-hunk "diff-hl-show-hunk" "\
+Show the VC diff hunk at point.
+The backend is determined by `diff-hl-show-hunk-function'." t nil)
+
+(autoload 'diff-hl-show-hunk-mouse-mode "diff-hl-show-hunk" "\
+Enables the margin and fringe to show a posframe/popup with vc diffs when clicked.
+By default, the popup shows only the current hunk, and
+the line of the hunk that matches the current position is
+highlighted.  The face, border and other visual preferences are
+customizable.  It can be also invoked with the command
+`diff-hl-show-hunk'
+\\{diff-hl-show-hunk-mouse-mode-map}
+
+If called interactively, enable Diff-Hl-Show-Hunk-Mouse mode if
+ARG is positive, and disable it if ARG is zero or negative.  If
+called from Lisp, also enable the mode if ARG is omitted or nil,
+and toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(put 'global-diff-hl-show-hunk-mouse-mode 'globalized-minor-mode t)
+
+(defvar global-diff-hl-show-hunk-mouse-mode nil "\
+Non-nil if Global Diff-Hl-Show-Hunk-Mouse mode is enabled.
+See the `global-diff-hl-show-hunk-mouse-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `global-diff-hl-show-hunk-mouse-mode'.")
+
+(custom-autoload 'global-diff-hl-show-hunk-mouse-mode "diff-hl-show-hunk" nil)
+
+(autoload 'global-diff-hl-show-hunk-mouse-mode "diff-hl-show-hunk" "\
+Toggle Diff-Hl-Show-Hunk-Mouse mode in all buffers.
+With prefix ARG, enable Global Diff-Hl-Show-Hunk-Mouse mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Diff-Hl-Show-Hunk-Mouse mode is enabled in all buffers where
+`diff-hl-show-hunk-mouse-mode' would do it.
+See `diff-hl-show-hunk-mouse-mode' for more information on Diff-Hl-Show-Hunk-Mouse mode.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-show-hunk" '("diff-hl-show-hunk-")))
+
+;;;***
+
+;;;### (autoloads nil "diff-hl-show-hunk-posframe" "diff-hl-show-hunk-posframe.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from diff-hl-show-hunk-posframe.el
+
+(autoload 'diff-hl-show-hunk-posframe "diff-hl-show-hunk-posframe" "\
+Implementation to show the hunk in a posframe.
+
+\(fn BUFFER &optional LINE)" nil nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "diff-hl-show-hunk-posframe" '("diff-hl-show-hunk-")))
 
 ;;;***
 
