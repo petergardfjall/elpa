f9a631abc8f65e1e92f323925c7281f3fd2cc85d
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-03-15 10:28:55 +0100

    powerline-20201129.2306 -> powerline-20210313.1621

---
+++ powerline-20210313.1621/powerline-pkg.el
@@ -0,0 +1,12 @@
+(define-package "powerline" "20210313.1621" "Rewrite of Powerline"
+  '((cl-lib "0.2"))
+  :commit "92525ddf9acb6518f33b4503237bc4cf9721969f" :authors
+  '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
+  :maintainer
+  '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
+  :keywords
+  '("mode-line")
+  :url "http://github.com/milkypostman/powerline/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
