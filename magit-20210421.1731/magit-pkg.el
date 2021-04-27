b72e9d81a426792c166e69ae559c6733c0b26eb9
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-04-27 07:36:42 +0200

    magit-20210411.2036 -> magit-20210421.1731

--- magit-20210421.1731/magit-pkg.el
+++ magit-20210421.1731/magit-pkg.el
@@ -1,10 +1,10 @@
-(define-package "magit" "20210411.2036" "A Git porcelain inside Emacs."
+(define-package "magit" "20210421.1731" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "5882df245d3388cd6f443bc11df219a838104df2" :authors
+  :commit "f883b62fedd42eaabace946336d61457211cc5ff" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
