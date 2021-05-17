fc64a41013fc0705f4c24e30d43289515be37e9f
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-05-17 08:29:57 +0200

    lsp-java-20210426.1238 -> lsp-java-20210506.829

--- lsp-java-20210506.829/lsp-java-pkg.el
+++ lsp-java-20210506.829/lsp-java-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-java" "20210426.1238" "Java support for lsp-mode"
+(define-package "lsp-java" "20210506.829" "Java support for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "6.0")
     (markdown-mode "2.3")
@@ -8,7 +8,7 @@
     (request "0.3.0")
     (treemacs "2.5")
     (dap-mode "0.5"))
-  :commit "213fc416ed7e68c07c9e84b320a50eceb40b5f05" :keywords
+  :commit "ba347ab575e17e7fb101101ac7537dd0df6e55d9" :keywords
   '("languague" "tools")
   :url "https://github.com/emacs-lsp/lsp-java")
 ;; Local Variables:
