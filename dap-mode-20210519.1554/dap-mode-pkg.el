bc8060f6bdddccd39f99266e0cfed4e3a38a54cd
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-06-10 07:47:11 +0200

    dap-mode-20210506.1934 -> dap-mode-20210519.1554

--- dap-mode-20210519.1554/dap-mode-pkg.el
+++ dap-mode-20210519.1554/dap-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dap-mode" "20210506.1934" "Debug Adapter Protocol mode"
+(define-package "dap-mode" "20210519.1554" "Debug Adapter Protocol mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
@@ -8,7 +8,7 @@
     (lsp-treemacs "0.1")
     (posframe "0.7.0")
     (ht "2.3"))
-  :commit "49af1b8cbd261a5f97e1b2886418dae5e51b452d" :authors
+  :commit "cc395e066755c7513d4862f5639f3d162b3bd30f" :authors
   '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
   :maintainer
   '("Ivan Yonchovski" . "yyoncho@gmail.com")
