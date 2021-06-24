1dc4ea60adbe24be6edac43642e2b06b479007ad
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-06-24 08:00:16 +0200

    posframe-20210604.647 -> posframe-20210617.42

--- posframe-20210617.42/posframe-pkg.el
+++ posframe-20210617.42/posframe-pkg.el
@@ -1,6 +1,6 @@
-(define-package "posframe" "20210604.647" "Pop a posframe (just a frame) at point"
+(define-package "posframe" "20210617.42" "Pop a posframe (just a frame) at point"
   '((emacs "26"))
-  :commit "3257a52e3599bc1d5c9a4b646d36d7a49b0dd025" :authors
+  :commit "f97c4aff2c2c376ca62276d5597aa108546633a9" :authors
   '(("Feng Shu" . "tumashu@163.com"))
   :maintainer
   '("Feng Shu" . "tumashu@163.com")
