2caad35d3c7fc28075f2bdd94c3bafa3f32f13a2
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-07-11 14:54:57 +0200

    dash-20210609.1330 -> dash-20210708.2009

--- dash-20210708.2009/dash-autoloads.el
+++ dash-20210708.2009/dash-autoloads.el
@@ -20,6 +20,9 @@ If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
 the mode if ARG is nil, omitted, or is a positive number.
 Disable the mode if ARG is a negative number.
 
+To check whether the minor mode is enabled in the current buffer,
+evaluate `dash-fontify-mode'.
+
 The mode's hook is called both when the mode is enabled and when
 it is disabled.
 
--- dash-20210708.2009/dash-pkg.el
+++ dash-20210708.2009/dash-pkg.el
@@ -1,6 +1,6 @@
-(define-package "dash" "20210609.1330" "A modern list library for Emacs"
+(define-package "dash" "20210708.2009" "A modern list library for Emacs"
   '((emacs "24"))
-  :commit "88d799595e8f1b4154637ce8a3f81b97b0520c1a" :authors
+  :commit "2675596b9ac1c4b9d47b93e227f06f8ec6755ec6" :authors
   '(("Magnar Sveen" . "magnars@gmail.com"))
   :maintainer
   '("Magnar Sveen" . "magnars@gmail.com")
