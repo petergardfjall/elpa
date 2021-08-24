4a84400c7263bdf473d5cd85af047ee976d05efc
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-08-24 07:52:43 +0200

    magit-section-20210615.1036 magit-section-20210702.822 -> magit-section-20210819.1119

--- magit-section-20210819.1119/magit-section-pkg.el
+++ magit-section-20210819.1119/magit-section-pkg.el
@@ -1,7 +1,7 @@
-(define-package "magit-section" "20210702.822" "Sections for read-only buffers"
+(define-package "magit-section" "20210819.1119" "Sections for read-only buffers"
   '((emacs "25.1")
-    (dash "20210330"))
-  :commit "9b7b25f1e460f79de894f583727635134b7513a2" :authors
+    (dash "2.18.1"))
+  :commit "8811d42eab06964f3169b9c15cf1cc1314b8703c" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
