e255178e53795643a63c66d1831962833c56a5d0
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-09-23 20:02:15 +0200

    lsp-ui-20210820.1331 -> lsp-ui-20210904.2037

--- lsp-ui-20210904.2037/lsp-ui-pkg.el
+++ lsp-ui-20210904.2037/lsp-ui-pkg.el
@@ -1,9 +1,9 @@
-(define-package "lsp-ui" "20210820.1331" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20210904.2037" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "5d643fbb0c4ef5fc4ee93d9894bf68388095160a" :authors
+  :commit "b625f3cb5e88559ab99bec58f7a14272edb296bc" :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
   :maintainer
   '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
