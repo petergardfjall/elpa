45fd423bf6d6693e6877c114175a4dc5b22a7ef9
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-10-20 07:46:25 +0200

    git-commit-20211008.112 -> git-commit-20211010.1635

--- git-commit-20211010.1635/git-commit-pkg.el
+++ git-commit-20211010.1635/git-commit-pkg.el
@@ -1,9 +1,9 @@
-(define-package "git-commit" "20211008.112" "Edit Git commit messages"
+(define-package "git-commit" "20211010.1635" "Edit Git commit messages."
   '((emacs "25.1")
-    (dash "2.19.1")
-    (transient "0.3.6")
-    (with-editor "3.0.5"))
-  :commit "ac1b581fc43798051030f28ae25737249d661472" :authors
+    (dash "20210826")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "aba0a596115b42fbd60347d893bcc319020ce5a2" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li")
     ("Sebastian Wiesner" . "lunaryorn@gmail.com")
     ("Florian Ragwitz" . "rafl@debian.org")
