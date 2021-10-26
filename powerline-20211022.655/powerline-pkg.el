50aeec7cbe4eaa17dad8d87f3e1ebc96e7ef0dd1
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-10-26 08:09:50 +0200

    powerline-20210527.1953 -> powerline-20211022.655

--- powerline-20211022.655/powerline-pkg.el
+++ powerline-20211022.655/powerline-pkg.el
@@ -1,6 +1,6 @@
-(define-package "powerline" "20210527.1953" "Rewrite of Powerline"
+(define-package "powerline" "20211022.655" "Rewrite of Powerline"
   '((cl-lib "0.2"))
-  :commit "cfba2aa9cfa190e7720900f01a946e1e78aac7e2" :authors
+  :commit "390a95fe5b71cfc20e18d034b4b35b5c159a83fc" :authors
   '(("Donald Ephraim Curtis" . "dcurtis@milkbox.net"))
   :maintainer
   '("Donald Ephraim Curtis" . "dcurtis@milkbox.net")
