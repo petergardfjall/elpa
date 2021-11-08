c51d44f3a43a3cba13722851053a15126e343bf4
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-11-08 07:37:41 +0100

    ivy-posframe-20210922.24 -> ivy-posframe-20211103.233

--- ivy-posframe-20211103.233/ivy-posframe-autoloads.el
+++ ivy-posframe-20211103.233/ivy-posframe-autoloads.el
@@ -31,7 +31,7 @@ the mode if ARG is nil, omitted, or is a positive number.
 Disable the mode if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
-evaluate `(default-value 'ivy-posframe-mode)'.
+evaluate `(default-value \\='ivy-posframe-mode)'.
 
 The mode's hook is called both when the mode is enabled and when
 it is disabled.
