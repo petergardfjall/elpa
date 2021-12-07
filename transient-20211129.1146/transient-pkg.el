fd89f76ea72210394b007003b52aecb902fedc79
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-12-07 08:16:59 +0100

    transient-20211105.100 -> transient-20211129.1146

--- transient-20211129.1146/transient-pkg.el
+++ transient-20211129.1146/transient-pkg.el
@@ -1,6 +1,6 @@
-(define-package "transient" "20211105.100" "Transient commands"
+(define-package "transient" "20211129.1146" "Transient commands"
   '((emacs "25.1"))
-  :commit "349116159f707a474926b47e5f6b6240e8997a4d" :authors
+  :commit "4e8aa09b3f9b909dddde85269eb5051b5801a07c" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
