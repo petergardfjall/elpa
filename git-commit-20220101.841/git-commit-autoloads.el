618a7cbd847b1ce1420dd362a663d47c40f26499
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2022-01-20 13:09:30 +0100

    git-commit-20211204.2135 -> git-commit-20220101.841

--- git-commit-20220101.841/git-commit-autoloads.el
+++ git-commit-20220101.841/git-commit-autoloads.el
@@ -28,6 +28,6 @@
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
+;; coding: utf-8-emacs-unix
 ;; End:
 ;;; git-commit-autoloads.el ends here
--- git-commit-20220101.841/git-commit-pkg.el
+++ git-commit-20220101.841/git-commit-pkg.el
@@ -1,9 +1,9 @@
-(define-package "git-commit" "20211204.2135" "Edit Git commit messages."
+(define-package "git-commit" "20220101.841" "Edit Git commit messages."
   '((emacs "25.1")
     (dash "20210826")
     (transient "20210920")
     (with-editor "20211001"))
-  :commit "74b84b57cc6aca261392e9189e8e6c5e9cc19ca7" :authors
+  :commit "2a7812705f5cf4b864bb162576415863f4116285" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li")
     ("Sebastian Wiesner" . "lunaryorn@gmail.com")
     ("Florian Ragwitz" . "rafl@debian.org")
